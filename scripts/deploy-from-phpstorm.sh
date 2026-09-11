3045b528132fc301393e2f44225826d6247a5721
timgabaree <timgabaree@outlook.com>
2026-09-11 17:15:36 -0500

    Add deploy from phpstorm script

--- scripts/deploy-from-phpstorm.sh
+++ scripts/deploy-from-phpstorm.sh
@@ -26,7 +26,5 @@ fi
 echo "==> Local repository matches GitHub"
 echo "==> Starting production deployment"
 
-ssh -T \
-    -i ~/.ssh/timgabaree_com \
-    ma81y7v45nyj@ssh.timgabaree.com \
+ssh -T ssh.timgabaree.com \
     "cd ~/repositories/timgabaree.com && ./scripts/deploy-production.sh"
